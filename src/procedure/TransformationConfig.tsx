e9bee3b961f94f9ab2bee83d7f1292180d569fd2
Diego de la Hera <delahera@gmail.com>
2022-07-08 15:15:21 -0300

    Add TransformationConfig component

--- src/procedure/TransformationConfig.tsx
+++ src/procedure/TransformationConfig.tsx
@@ -1,98 +1,94 @@
 import {
-    Autocomplete,
-    Box,
-    MenuItem,
-    Select,
-    TextField,
-  } from "@mui/material";
-  import React from "react";
-  import { useTranslation } from 'react-i18next';
-  import { SelectionStep } from '../types';
-  import { camelToKebabCase } from '../utils';
-  import { transformations } from '../config';
-  
-  interface TransformationConfigComponentProps {
-    fieldname: string;
-    type: string;
-    args: {
-      value: string;
-      error?: Error;
-    }[]
-    itemwise: boolean;
-  }
-  
-  function TransformationConfigComponent(props: TransformationConfigComponentProps) {
-    const { t } = useTranslation();
-  
-    const transformation = transformations.filter(
-      (transformation) => transformation.type === props.type
-    )[0];
-  
-    return (
-      <Box
-        sx={{
-          display: "flex",
-          gap: "1em"
-        }}
-      >
-      <Select
-        value={props.type}
-        label={t('transformation-config.type')}
-        size="small"
-      >
-      {
-        selections.map((selection) => (
-          <MenuItem
-            value={selection.type}
-          >{t(camelToKebabCase(`selection.${selection.type}.label`))}
-          </MenuItem>
-        ))
+  Box,
+  IconButton,
+  MenuItem,
+  Select,
+  Tooltip
+} from "@mui/material";
+import { FilterNone } from '@mui/icons-material'
+import StepConfigParam from './StepConfigParam';
+import React from "react";
+import { useTranslation } from 'react-i18next';
+import { TransformationStep } from '../types';
+import { camelToKebabCase } from '../utils';
+import { transformations } from '../config';
+
+interface TransformationConfigComponentProps {
+  fieldname: string;
+  type: string;
+  args: {
+    value: string;
+    error?: Error;
+  }[]
+  itemwise: boolean;
+}
+
+function TransformationConfigComponent(props: TransformationConfigComponentProps) {
+  const { t } = useTranslation();
+
+  const transformation = transformations.filter(
+    (transformation) => transformation.type === props.type
+  )[0];
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        gap: "1em"
+      }}
+    >
+    <Select
+      value={props.type}
+      label={t('transformation-config.type')}
+      size="small"
+    >
+    {
+      transformations.map((transformation) => (
+        <MenuItem
+          value={transformation.type}
+        >{t(camelToKebabCase(`selection.${transformation.type}.label`))}
+        </MenuItem>
+      ))
+    }
+    </Select>
+    {
+      transformation.config.map((param, index) => {
+        const { name, options } = param;
+        const fieldOptions: string[] = [];
+        const { value, error } = props.args[index];
+
+        return (
+          <StepConfigParam
+            label={t(camelToKebabCase(
+              `selection.${props.type}.config.${name}.label`
+            ))}
+            options={fieldOptions.length ? fieldOptions : options}
+            prefix={
+              fieldOptions.length ?
+              `field.${props.fieldname}` :
+              `selection.${props.type}.config.${name}`
+            }
+            value={value}
+            error={error}
+          />
+        )
+      })
+    }
+    <Tooltip
+      title={
+        t(
+          props.itemwise ?
+          'transformation-config.itemwise.disable' :
+          'transformation-config.itemwise.enable'
+        )
       }
-      </Select>
-      {
-        selection.config.map((param, index) => {
-          const { name, options } = param;
-          const fieldOptions: string[] = [];
-          const { value, error } = props.args[index];
-          
-          const renderInput = (inputParams) => {
-            return (
-              <TextField
-                {...inputParams}
-                sx={{ flex: 1 }}
-                label={name}
-                value={value}
-                size="small"
-              />
-            )
-          }
-  
-          if (fieldOptions.length || options.length) {
-            return(
-              <Autocomplete
-                sx={{ flex: 1 }}
-                options={
-                  fieldOptions.length ?
-                  fieldOptions :
-                  options
-                }
-                getOptionLabel={(option) => (
-                  t(camelToKebabCase(
-                    fieldOptions.length ?
-                    `field.${props.fieldname}.option.${option}` :
-                    `selection.${props.type}.config.${name}.option.${option}`
-                  )) + ` (${option})`
-                )}
-                renderInput={renderInput}
-              />
-            )
-          } else {
-            return renderInput({});
-          }
-        })
-      }
-      </Box>
-    )
-  }
-  
-  export default SelectionConfigComponent;
+    >
+      <IconButton>
+        <FilterNone />
+      </IconButton>
+    </Tooltip>
+    </Box>
+  )
+}
+
+export default SelectionConfigComponent;
